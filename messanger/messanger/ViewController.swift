caddbd1beaca9b5869151d0ccfa6315e6c8c3e87
Evan <ries8898@gmial.com>
2018-12-10 00:39:39 +0300

    first steps
    getting user info to console

--- messanger/messanger/ViewController.swift
+++ messanger/messanger/ViewController.swift
@@ -7,11 +7,31 @@
 //
 
 import UIKit
+import Alamofire
+import SwiftyJSON
+
+let AccessToken = "eaab198bb6705808f525ad2a81b0300228778ed6488e60888cd9a55957b3ab31c702394787510f5ad3c34"
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        //------------------
+        //let url: String = "https://httpbin.org/get"
+        var url: String = "https://api.vk.com/method/users.get?user_id=210700286&v=5.52"
+        url += "&access_token=\(AccessToken)"
+        
+        AF.request(url, method: .get).validate().responseJSON {
+            response in
+            
+            switch response.result {
+            case .success(let value):
+                let json = JSON(value)
+                print("JSON: \(json)")
+            case .failure(let error):
+                print(error)
+            }
+        }
         // Do any additional setup after loading the view, typically from a nib.
     }
 
