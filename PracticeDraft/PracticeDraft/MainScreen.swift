82fb931aa1643a137da0b02f05f1fd6bcfa9c08c
Evan <ries8898@gmial.com>
2019-07-16 20:59:59 +0300

    jsonSerialization doesn't conform the Cyrillic

--- PracticeDraft/PracticeDraft/MainScreen.swift
+++ PracticeDraft/PracticeDraft/MainScreen.swift
@@ -28,7 +28,20 @@ class MainScreen: UIViewController, UITableViewDelegate, UITableViewDataSource {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-       
+        guard let path = Bundle.main.path(forResource: "items", ofType: "json")
+            else {return}
+        let url = URL(fileURLWithPath: path)
+        
+        do {
+            let data = try Data(contentsOf: url)
+            let json = try JSONSerialization.jsonObject(with: data, options: .mutableContainers)
+            
+            print(json) 
+        }
+        catch {
+            print(error) // if error represented
+        }
+        
     }
     
 
