b99b4d931f31c7117a4e322877b4243cbc2a04f2
b0dah <ries8898@gmail.com>
2019-11-19 23:51:35 +0300

    request to Dima's server works!

--- Requester/Requester/ViewController.swift
+++ Requester/Requester/ViewController.swift
@@ -14,34 +14,35 @@ class ViewController: UIViewController {
     
     @IBAction func didSendButtonClick(_ sender: UIButton) {
         
-        guard let initialUrl = URL(string: "https://api.nasa.gov/planetary/apod") else {
+        guard let initialUrl = URL(string: "http://localhost:8080/warning_vhodit_krisa") else {
             print("wrong url")
             return
         }
         
-        let parameters: [String : String] = [ "api_key" : "DEMO_KEY",
-                                              "date" : "2011-07-13"]
-        
-        var request = URLRequest(url: initialUrl)
-        request.httpMethod = "POST"
+        let parameters: [String : String] = [ "login" : "DEMO_KEY",
+                                              "password" : "DEMO_KEY"]
+        let urlWithParameters = initialUrl.withQueries(parameters)!
+                                            
+        var request = URLRequest(url: urlWithParameters)
+        request.httpMethod = "GET"
         request.setValue("Application/json", forHTTPHeaderField: "Content-Type")
-        guard let httpBody = try? JSONSerialization.data(withJSONObject: parameters, options: []) else {
-            print("wrong parameters")
-            return
-        }
-        request.httpBody = httpBody
+//        guard let httpBody = try? JSONSerialization.data(withJSONObject: parameters, options: []) else {
+//            print("wrong parameters")
+//            return
+//        }
+//        request.httpBody = httpBody
        
         
         let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
             if let data = data{
                 
                 do {
-                    if let json = try JSONSerialization.jsonObject(with: data, options: []) as? [String : Any],
-                        let name = json["Name"] as? String {
-                            print(json)
-                            DispatchQueue.main.async {
-                                self.responseLabel.text = name
-                        }
+                    if let json = try JSONSerialization.jsonObject(with: data, options: []) as? [String : Any] {//,
+//                        let name = json["Name"] as? String {
+//                            print(json)
+//                            DispatchQueue.main.async {
+//                                self.responseLabel.text = name
+//                        }
                     }
                 } catch {
                     print("*** that's error", error)
